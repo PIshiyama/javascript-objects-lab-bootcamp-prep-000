370d9b3afd1852c5a56660c27f4757523b6d9212
Paul Ishiyama <paul.h.ishiyama@gmail.com>
2018-08-16 00:10:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
-function updateOpjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
